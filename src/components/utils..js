e4b65feaa30c7c8829d83f8c9cfaeb40146fd9de
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-01-06 13:02:00 +0530

    added backend url

--- src/components/utils..js
+++ src/components/utils..js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://mernauthentication.vercel.app";
-export const BACKEND_URL = "http://127.0.0.1:5000";
+export const BACKEND_URL = "https://mernauthentication.vercel.app";
+// export const BACKEND_URL = "http://127.0.0.1:5000";
